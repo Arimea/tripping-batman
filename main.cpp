8a04d409f6dd584b7fe9681c54624f288301ee38
Miika <miika.puljujarvi@gmail.com>
2013-11-28 23:19:59 +0200

    hitech

--- main.cpp
+++ main.cpp
@@ -33,12 +33,13 @@ int main()
 
     //MUUTTUJIA
     Vector2f sijainti;
-    float rotaatio;
+    //float rotaatio;
+    int palloSkaala=0;
 
     //Luodaan ikkuna
     RenderWindow window(VideoMode(800, 600), "Pelitesti");
 
-
+    Time skaalasekunti = milliseconds(100.f);
     Time millisekunti = milliseconds(3.f);
 
     //Tausta eli kentta, jolla liikutaan
@@ -54,11 +55,13 @@ int main()
     piirraesteet.setTexture(esteet);
 
     //vihrea objekti, jota voi liikuttaa kentalla
-    Sprite vihree;
-    vihree.setTexture(tausta);
-    vihree.setColor(Color(0, 255, 0));
-    vihree.setScale(0.035, 0.05); //28x30, alkup. 800x600
-    vihree.setOrigin(400,300);//eli onnistui
+    Texture pallo;
+    pallo.loadFromFile("img/pallohitech.png");
+    Sprite piirrapallo;
+    piirrapallo.setTexture(pallo);
+    //piirrapallo.setColor(Color(0, 255, 0));
+    piirrapallo.setScale(0.4, 0.4); //28x30, alkup. 800x600
+    piirrapallo.setOrigin(100,100);//eli onnistui
 
 
 
@@ -69,11 +72,40 @@ int main()
         }
         //vihree=rotation(vihree);
         //rotaatio=vihree.getRotation();
-        if(Keyboard::isKeyPressed(Keyboard::Right)){vihree.move(1,1);}
-        if(Keyboard::isKeyPressed(Keyboard::Left)){vihree.move(-1,0);}
-        if(Keyboard::isKeyPressed(Keyboard::Up)){vihree.move(0,-1);}
-        if(Keyboard::isKeyPressed(Keyboard::Down)){vihree.move(0,1);}
-        if(Keyboard::isKeyPressed(Keyboard::Space)){vihree.setPosition(400,300);}
+        if(Keyboard::isKeyPressed(Keyboard::Right))
+        {piirrapallo.move(0.5,0);piirrapallo.setRotation(90);}
+        if(Keyboard::isKeyPressed(Keyboard::Left))
+            {piirrapallo.move(-0.5,0);piirrapallo.setRotation(270);}
+        if(Keyboard::isKeyPressed(Keyboard::Up))
+        {piirrapallo.move(0,-0.5);piirrapallo.setRotation(0);}
+        if(Keyboard::isKeyPressed(Keyboard::Down))
+        {piirrapallo.move(0,0.5);piirrapallo.setRotation(180);}
+        if(Keyboard::isKeyPressed(Keyboard::Space))
+            {
+                if(palloSkaala==0)
+                {piirrapallo.setScale(0.6,0.6);
+                palloSkaala++;
+                sleep(skaalasekunti);}
+                else
+                {piirrapallo.setScale(0.3,0.3);
+                palloSkaala--;
+                sleep(skaalasekunti);}
+            }
+
+        if((Keyboard::isKeyPressed(Keyboard::Right)) && (Keyboard::isKeyPressed(Keyboard::Up)))
+            {piirrapallo.move(-0.15,0.15);
+            piirrapallo.setRotation(45);}
+        if((Keyboard::isKeyPressed(Keyboard::Right)) && (Keyboard::isKeyPressed(Keyboard::Down)))
+            {piirrapallo.move(-0.15,-0.15);
+            piirrapallo.setRotation(135);}
+        if((Keyboard::isKeyPressed(Keyboard::Left)) && (Keyboard::isKeyPressed(Keyboard::Up)))
+            {piirrapallo.move(0.15,0.15);
+            piirrapallo.setRotation(315);}
+        if((Keyboard::isKeyPressed(Keyboard::Left)) && (Keyboard::isKeyPressed(Keyboard::Down)))
+            {piirrapallo.move(0.15,-0.15);
+            piirrapallo.setRotation(225);}
+
+
         Event event;
         while (window.pollEvent(event))
         {
@@ -84,10 +116,10 @@ int main()
         window.clear();
         window.draw(piirratausta);
         window.draw(piirraesteet);
-        window.draw(vihree);
+        window.draw(piirrapallo);
         window.display();
         sleep(millisekunti);
     }
-
     return 0;
 }
+
