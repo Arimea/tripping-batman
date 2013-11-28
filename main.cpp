dc9e5e461540bbdf933fcd3c3c43b6909d58ab53
Miika <miika.puljujarvi@gmail.com>
2013-11-28 22:04:26 +0200

    pallo lisatty

--- main.cpp
+++ main.cpp
@@ -3,28 +3,62 @@
 #include <string>
 #include <stdio.h>
 
+sf::Sprite rotation(sf::Sprite kaannettava)
+{
+
+    using namespace sf;
+    Time millisekunti = milliseconds(3.f);
+
+    if(Keyboard::isKeyPressed(Keyboard::Left))
+    {
+        //kaannettava.setRotation(kaannettava.getRotation()+1.f);
+        kaannettava.rotate(1.f);
+        sleep(millisekunti);
+    }
+
+    if(Keyboard::isKeyPressed(Keyboard::Right))
+    {
+       //kaannettava.setRotation(kaannettava.getRotation()-1.f);
+       kaannettava.rotate(-1.f);
+       sleep(millisekunti);
+    }
+    return kaannettava;
+}
+
+
 int main()
 {
+
     using namespace sf;
-    //printf(2)
+
+    //MUUTTUJIA
+    Vector2f sijainti;
+    float rotaatio;
+
     //Luodaan ikkuna
     RenderWindow window(VideoMode(800, 600), "Pelitesti");
-
 
 
     Time millisekunti = milliseconds(3.f);
 
     //Tausta eli kentta, jolla liikutaan
     Texture tausta;
-    tausta.loadFromFile("img/Pelitausta.jpg");
+    tausta.loadFromFile("img/Pelitausta_tyhja.jpg");
     Sprite piirratausta;
     piirratausta.setTexture(tausta);
+
+    //Ruskeat esteet laudalla
+    Texture esteet;
+    esteet.loadFromFile("img/Esteet.png");
+    Sprite piirraesteet;
+    piirraesteet.setTexture(esteet);
 
     //vihrea objekti, jota voi liikuttaa kentalla
     Sprite vihree;
     vihree.setTexture(tausta);
     vihree.setColor(Color(0, 255, 0));
-    vihree.setScale(0.035, 0.05);
+    vihree.setScale(0.035, 0.05); //28x30, alkup. 800x600
+    vihree.setOrigin(400,300);//eli onnistui
 
 
 
@@ -33,12 +67,13 @@ int main()
         if(Keyboard::isKeyPressed(Keyboard::Escape)){
             window.close();
         }
-
-        if(Keyboard::isKeyPressed(Keyboard::Right)){vihree.move(1,0);}
+        //vihree=rotation(vihree);
+        //rotaatio=vihree.getRotation();
+        if(Keyboard::isKeyPressed(Keyboard::Right)){vihree.move(1,1);}
         if(Keyboard::isKeyPressed(Keyboard::Left)){vihree.move(-1,0);}
         if(Keyboard::isKeyPressed(Keyboard::Up)){vihree.move(0,-1);}
         if(Keyboard::isKeyPressed(Keyboard::Down)){vihree.move(0,1);}
-
+        if(Keyboard::isKeyPressed(Keyboard::Space)){vihree.setPosition(400,300);}
         Event event;
         while (window.pollEvent(event))
         {
@@ -48,6 +83,7 @@ int main()
 
         window.clear();
         window.draw(piirratausta);
+        window.draw(piirraesteet);
         window.draw(vihree);
         window.display();
         sleep(millisekunti);
